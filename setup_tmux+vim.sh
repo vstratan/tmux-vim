58feb2a69ae2f56f196bf26e651f8548d4a4c868
Galbenus Adrian <adrian.galbenus@kaazing.com>
2016-12-19 17:39:25 +0200

    Update setup_tmux+vim.sh

--- setup_tmux+vim.sh
+++ setup_tmux+vim.sh
@@ -4,7 +4,25 @@
 # Figure out directory this script resides in
 exedir=$(readlink -f $(dirname $0))
 
+vimInstalled=$(which vim)
+if [ "$dockerInstalled" == "/usr/bin/vim" ]; then
+    echo "Vim is already installed."
+else
+set -e
+sudo apt-get -y install vim
+set +e
+fi
+tmuxInstalled=$(which tmux)
+if [ "$dockerInstalled" == "/usr/bin/tmux" ]; then
+    echo "Vim is already installed."
+else
+set -e
+sudo apt-get -y install tmux
+set +e
+fi
+
 cp {.vim,.vimrc,.tmux.conf} ~/
 mkdir ~/local
-cp tmux.sh ~/local/tmux
+cp tmux.sh ~/local/tmux/
+
 vim +PluginInstall
